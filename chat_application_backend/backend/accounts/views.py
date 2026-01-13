1f86518b2cbd2c91d2e5033c80eeec0ae3082215
Mohamed Kassim Sinan <mohamed.kassim.sinan@gmail.com>
2026-01-13 10:10:31 +0400

    First Commit

---
+++ chat_application_backend/backend/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
+# Create your views here.
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response(serializer.data, status=201)
+
+    return Response(serializer.errors, status=400)
